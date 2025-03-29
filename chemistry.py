b590f39ae79829afe39f6067286eb1199c096552
luciana-tech <luciana.lds@gmail.com>
2025-03-29 18:40:45 -0300

    Main funtion changed

--- chemistry.py
+++ chemistry.py
@@ -102,18 +102,63 @@ def make_periodic_table():
     return periodic_table_dict
 
 def main():
-    '''Get a chemical formula for a molecule from the user.'
-    '''
+    #Get a chemical formula for a molecule from the user.
     chemical_formula = (input("Please enter chemical formula for a molecule: "))
-    '''Get the mass of a chemical sample in grams from the user.'
-    '''
+
+    #Get the mass of a chemical sample in grams from the user.'
     mass = float(input("Enter the mass of the sample in grams: "))
-    '''Call make_periodic_table function and store the returned list'
-    '''
-    periodic_table = make_periodic_table()
+    
+    #Call make_periodic_table function and store the returned list
+    periodic_table_dict = make_periodic_table()
+
+    #Call the parse_formula function to convert the chemical formula given by the user to a compound list that stores element symbols and the quantity of atoms of each element in the molecule.
+    symbol_quantity_list = parse_formula(chemical_formula, periodic_table_dict)
+
+    #Compute the molar mass
+    molar_mass = compute_molar_mass(symbol_quantity_list, periodic_table_dict)
+    
+    #Compute numbers of moles in the sample
+    moles = mass / molar_mass
 
     for element in periodic_table:
         print(f"{element[1]}: {element[2]} amu")
+
+# Indexes for inner lists in the periodic table
+NAME_INDEX = 0
+ATOMIC_MASS_INDEX = 1
+# Indexes for inner lists in a symbol_quantity_list
+SYMBOL_INDEX = 0
+QUANTITY_INDEX = 1
+def compute_molar_mass(symbol_quantity_list, periodic_table_dict):
+    """Compute and return the total molar mass of all the
+    elements listed in symbol_quantity_list.
+    Parameters
+        symbol_quantity_list is a compound list returned
+            from the parse_formula function. Each small
+            list in symbol_quantity_list has this form:
+            ["symbol", quantity].
+        periodic_table_dict is the compound dictionary
+            returned from make_periodic_table.
+    Return: the total molar mass of all the elements in
+        symbol_quantity_list.
+    For example, if symbol_quantity_list is [["H", 2], ["O", 1]],
+    this function will calculate and return
+    atomic_mass("H") * 2 + atomic_mass("O") * 1
+    1.00794 * 2 + 15.9994 * 1
+    18.01528
+    """
+    # Do the following for each inner list in the
+    # compound symbol_quantity_list:
+        # Separate the inner list into symbol and quantity.
+        # Get the atomic mass for the symbol from the dictionary.
+        # Multiply the atomic mass by the quantity.
+        # Add the product into the total molar mass.
+    # Return the total molar mass.
+    total_mass = 0
+    for symbol, quantity in symbol_quantity_list:
+        atomic_mass = periodic_table_dict[symbol][1]
+        total_mass += * quantity
+    return total_mass
 
 if __name__ == "__main__":
     main()     
